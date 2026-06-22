0221ba6d7c71e9727191ed6087d07db8b704460e
Bintan <siregarbintan@gmail.com>
2026-06-23 05:01:55 +0800

    Add details

--- src/core/mahameru-http-server-error.ts
+++ src/core/mahameru-http-server-error.ts
@@ -1,16 +1,14 @@
-export class MahameruHttpServerError {
-    public readonly name: string = 'MahameruHttpServerError';
-    public readonly stack?: string;
+export class MahameruHttpServerError extends Error {
+    public readonly statusCode: number = 500;
+    public readonly details?: string;
 
-    constructor(public message: string) {
+    constructor(details?: string) {
+        super('Internal Server Error');
+        this.name = 'MahameruHttpServerError';
+        this.details = details;
+
         if (Error.captureStackTrace) {
             Error.captureStackTrace(this, MahameruHttpServerError);
-        } else {
-            this.stack = (new Error()).stack;
         }
-    }
-
-    toString(): string {
-        return `${this.name}: ${this.message}`;
     }
 }
