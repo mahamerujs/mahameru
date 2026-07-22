3cc08bd92cc05cc103f993fb970e735f8e21e6e4
Bintan <siregarbintan@gmail.com>
2026-07-22 15:07:52 +0800

    Resetting eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,24 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 
 export default tseslint.config(
-    {
-        ignores: ['**/dist/**', '**/node_modules/**', '**/coverage/**'],
+  {
+    ignores: ['**/dist/**', '**/node_modules/**', '**/coverage/**'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettier,
+  {
+    files: ['**/*.json', '**/*.ts', '**/*.tsx', '**/*.js', '**/*.mjs'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
     },
-    eslint.configs.recommended,
-    ...tseslint.configs.recommended,
-    eslintConfigPrettier,
-    {
-        files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.mjs'],
-        rules: {
-            'semi': ['error', 'always'],
-            '@typescript-eslint/no-unused-vars': [
-                'warn',
-                { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }
-            ],
-            'no-console': ['warn', { allow: ['warn', 'error'] }],
-            '@typescript-eslint/explicit-function-return-type': 'off',
-            '@typescript-eslint/no-explicit-any': 'warn',
-        },
-    }
+  },
 );
